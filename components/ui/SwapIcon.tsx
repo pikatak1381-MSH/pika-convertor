587e64c4316fd9caa034fb0834eee93f4a3cab20
pikatak1381-MSH <pikatak1381@gmail.com>
2025-12-16 16:19:13 +0330

    Update: General styles and Minor tweaks - Import Shadcn Select menu instead of html native - Add favicon and manifest file plus it's setup in root layout - Convert common icons into React components - Add new colors for inputs and input borders in globals.css

---
+++ components/ui/SwapIcon.tsx
@@ -0,0 +1,20 @@
+const SwapIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    width="17"
+    height="21"
+    viewBox="0 0 17 21"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M10.5 0.75L15.75 6L10.5 11.25M14.947 6H0.75M6 20.25L0.75 15L6 9.75M1.59375 15H15.75"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export default SwapIcon
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import { NextConfig } from "next"
 import createNextIntlPlugin from "next-intl/plugin"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  webpack(config) {
+    // Adding SVG loader for React components
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+}
 
 const withNextIntl = createNextIntlPlugin()
 export default withNextIntl(nextConfig)
